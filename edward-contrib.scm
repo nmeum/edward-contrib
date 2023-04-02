01553175bac527b201839440025c9d3b6b11f795
Sören Tempel <soeren+git@soeren-tempel.net>
2023-04-02 09:30:08 +0200

    Add an FZF command implementation for changing the current file

--- edward-contrib.scm
+++ edward-contrib.scm
@@ -1,4 +1,5 @@
 (import (scheme base)
+        (chicken process)
 
         (edward cli)
         (edward parse)
@@ -10,7 +11,22 @@
 
 (include "util.scm")
 
-;; Executor for the 'z' command.
+;;;;
+;; The FZF command.
+;;;;
+
+(define (exec-fzf editor)
+  (let* ((cmd (string-append "!" "fzf"))
+         (out (caar (read-from cmd))))
+    (%exec-edit editor out)))
+
+(define-file-cmd (fzf exec-fzf)
+  (parse-cmd-char #\F))
+
+;;;;
+;; The scroll command.
+;;;;
+
 (define (exec-scroll editor start %amount)
   (let* ((amount (if %amount
                    %amount
@@ -22,7 +38,6 @@
                                               (+ start amount)))))))
     (exec-print editor (cons start end))))
 
-;; Parser for the 'z' command.
 (define-file-cmd (scroll exec-scroll (make-addr '(current-line) '(+1)))
   (parse-cmd-char #\z)
   (parse-optional parse-number))
