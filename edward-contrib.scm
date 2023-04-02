de3fce0ba476595ec5d6c1559974001f0f74700a
Sören Tempel <soeren+git@soeren-tempel.net>
2023-04-02 23:43:54 +0200

    Various improvements of 'z' command implementation
    • Allow supplying an address
    • Default to the address of the next line
    • Take prompt lines into account when calculating amount

--- edward-contrib.scm
+++ edward-contrib.scm
@@ -11,9 +11,10 @@
 (include "util.scm")
 
 ;; Executor for the 'z' command.
-(define (exec-scroll editor %amount)
-  (let* ((amount (if %amount %amount (terminal-rows)))
-         (start  (addr->line editor (make-addr '(current-line))))
+(define (exec-scroll editor start %amount)
+  (let* ((amount (if %amount
+                   %amount
+                   (max 0 (- (terminal-rows) 2)))) ;; -2 for cur/nxt prompt
          (end    (addr->line editor (make-addr
                                       (cons 'nth-line
                                             (min
@@ -22,7 +23,7 @@
     (exec-print editor (cons start end))))
 
 ;; Parser for the 'z' command.
-(define-file-cmd (scroll exec-scroll)
+(define-file-cmd (scroll exec-scroll (make-addr '(current-line) '(+1)))
   (parse-cmd-char #\z)
   (parse-optional parse-digits))
 
